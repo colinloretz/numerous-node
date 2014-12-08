247521c5091f51f2af7c17753c8cc522f4decb9e
Colin Loretz <colin@cloudsnap.com>
2014-12-07 18:16:04 -0800

    added channel management endpoints

--- numerous.js
+++ numerous.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 function Numerous(api_key) {
-    this.url = 'https://api.numerousapp.com/v1';
+    this.url = 'https://api.numerousapp.com/';
     this.api_key = api_key;
 }
 
@@ -26,6 +26,41 @@ Numerous.prototype.makeRequest = function(verb, url, body, callback) {
         if (err) {
             return callback(err);
         } else if (res.statusCode !== 200 && res.statusCode !== 201) {
+            return callback(new Error(res.statusCode));
+        } else {
+            if (body) {
+                return callback(body);
+            } else {
+                return callback(null, body);
+            }
+        }
+    });
+}
+
+Numerous.prototype.makeChannelRequest = function(verb, url, body, token, callback) {
+	var self = this;
+	
+	if(body == undefined) {
+		body = "";
+	}
+	
+    var options = {
+        uri: url,
+        method: verb,
+        headers: {
+            'Authorization': 'Basic ' + new Buffer(self.api_key + ':').toString('base64'),
+			'Content-Type': 'application/json',
+			'X-Numerous-Token' : token
+        },
+		body: body
+    }
+	
+   request(options, function(err, res, body) {
+        if (err) {
+			console.log('Request error is ', err);
+            return callback(err);
+        } else if (res.statusCode !== 200 && res.statusCode !== 201) {
+			console.log('Request error is ', res.statusCode);
             return callback(new Error(res.statusCode));
         } else {
             if (body) {
@@ -81,6 +116,49 @@ Numerous.prototype.createError = function(metricId, commentBody, callback) {
 	var event = { kind: "error", commentBody: commentBody };
 	var jsonBody = JSON.stringify(event);
 	self.makeRequest("POST", self.url + '/metrics/' + metricId + '/interactions', jsonBody, callback)
+}
+
+/* ************************************** */
+/* Developer Channel Management Endpoints */
+/* ************************************** */
+
+Numerous.prototype.getChannels = function(channelId, callback) {
+	var self = this;
+	self.makeRequest("GET", + '/channels/' + channelId + '/metrics', callback);
+}
+
+Numerous.prototype.createChannel = function(channel, callback) {
+	var self = this;
+	self.makeRequest("POST", self.url + '/channels', channel, callback);
+}
+
+Numerous.prototype.getChannel = function(channelId, callback) {
+	var self = this;
+	self.makeRequest("GET", self.url + '/channels/' + channelId, callback);
+}
+
+Numerous.prototype.updateChannel = function(channel, callback) {
+	var self = this;
+	var channelData = JSON.parse(channel);
+	var channelId = channel.id
+	self.makeRequest("PUT", self.url + '/channels/' + channelId, channel, callback);
+}
+
+Numerous.prototype.updateChannel = function(channel, callback) {
+	var self = this;
+	var channelData = JSON.parse(channel);
+	var channelId = channel.id
+	self.makeRequest("PUT", self.url + '/channels/' + channelId, channel, callback);
+}
+
+Numerous.prototype.getChannelMetrics = function(channelId, callback) {
+	var self = this;
+	self.makeRequest("GET", self.url + '/channels/' + channelId + '/metrics', callback);
+}
+
+Numerous.prototype.createChannelMetric = function(metric, token, callback) {
+	var self = this;
+	self.makeChannelRequest("POST", self.url + '/metrics', metric, token, callback);
 }
 
 module.exports = Numerous;
